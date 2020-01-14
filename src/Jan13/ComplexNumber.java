825f9155bd27704942145d9411986841f0b05852
JYDM <johnwallar@gmail.com>
2020-01-13 23:19:39 -0500

    :)

---
+++ src/Jan13/ComplexNumber.java
@@ -0,0 +1,57 @@
+package Jan13;
+
+public class ComplexNumber {
+    private int a, b;
+    /*
+    In Mathematics, a complex number is a number that is composed of both a real component
+    and an imaginary component. Complex numbers can be expressed in the form a + bi where a and
+    b are real numbers and i is the imaginary number sqrt(-1). In a complex expression, a is
+    concidered the 'real part' and b is concidered the 'imaginary part'
+
+    You can add, subtract, multiply, and divide complex numbers. Addition with complex numbers
+    involves adding the real parts and the imaginary parts as two separate sums and expressing the
+    answer as a new complex number.
+
+    ... bla bla bla ...
+
+    Write the complete ComplexNumber class, including:
+     - Any necessary instance variables
+     - A constructor that takes two int variables, a and b, representing the components of the
+       complex number expressed a + bi
+     - The add does adding
+     - we get the rest
+
+
+     */
+
+    public int A() {
+        return a;
+    }
+
+    public int B() {
+        return b;
+    }
+
+    public ComplexNumber(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public ComplexNumber add(ComplexNumber toBeAdded) {
+        return new ComplexNumber(this.A() + toBeAdded.A(), this.B() + toBeAdded.B());
+    }
+
+    public static ComplexNumber mult(ComplexNumber first, ComplexNumber second) {
+        return new ComplexNumber(first.A() * second.A() - first.B() * second.B(),
+                first.A() * second.B() + first.B() * second.A());
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Jan13/Main.java
@@ -0,0 +1,23 @@
+package Jan13;
+
+public class Main {
+    /*
+    In Mathematics, a complex number is a number that is composed of both a real component
+    and an imaginary component. Complex numbers can be expressed in the form a + bi where a and
+    b are real numbers and i is the imaginary number sqrt(-1). In a complex expression, a is
+    concidered the 'real part' and b is concidered the 'imaginary part'
+
+    You can add, subtract, multiply, and divide complex numbers. Addition with complex numbers
+    involves adding the real parts and the imaginary parts as two separate sums and expressing the
+    answer as a new complex number.
+
+    ... bla bla bla ...
+
+    Write the complete ComplexNumber class, including:
+     - Any necessary instance variables
+     - A constructor that takes two int variables, a and b, representing the components of the
+       complex number expressed a + bi
+     - The add does adding
+     - we get the rest
+     */
+}
